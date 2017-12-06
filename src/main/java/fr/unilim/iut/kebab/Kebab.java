577167564d2f0734dc9638df9e77f8b9c7a1f43c
Nicolas Airault <nicolas.airault@etu.unilim.fr>
2017-12-06 10:42:01 +0100

    pattern decorator
    2 régimes, 10 ingrédients, 1 assiette

---
+++ src/main/java/fr/unilim/iut/kebab/Kebab.java
@@ -0,0 +1,12 @@
+package fr.unilim.iut.kebab;
+
+import java.util.List;
+import fr.unilim.iut.kebab.ingredients.Ingredient;
+
+public interface Kebab {
+	List<Ingredient> getIngredients();
+
+	boolean isVegetarien();
+
+	boolean isPescetarien();
+}
